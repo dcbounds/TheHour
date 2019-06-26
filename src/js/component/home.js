e0301f524b85bcc31fe986ef566a497765df45c1
dcbounds <boundsdarius@gmail.com>
2019-06-26 17:31:02 +0000

    Added react files back and the hour logo with a new navbar and home component

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,25 +1,25 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TheHour from "../../img/TheHourLogo.png";
+import Navbar from "./navbar.js";
 
 //create your first component
-export class Home extends React.Component {
+export default class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div>
+				<Navbar />
+				<div className="text-center mt-5">
+					<h1>THE HOUR</h1>
+					<p>
+						<img src={TheHour} />
+					</p>
+					<a href="#" className="btn btn-success">
+						BUTTON
+					</a>
+					<p>Made by The Hour Team</p>
+				</div>
 			</div>
 		);
 	}
---
+++ src/js/component/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//include images into your bundle
+import TheHour from "../../img/TheHourLogo.png";
+
+//create your first component
+export default class Navbar extends React.Component {
+	render() {
+		return <div className="text-center mt-5">navbar component</div>;
+	}
+}
